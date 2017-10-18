e2035fcd8781fe0696e7d3e62c9d587b44f1533f
Thong Le <thong.le@saigontechnology.vn>
2017-10-18 13:15:20 +0700

    add barcode scanner

---
+++ app/src/main/java/com/company/sts_ar/view/menu/BarcodeActivity.java
@@ -0,0 +1,61 @@
+package com.company.sts_ar.view.menu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+
+import com.company.sts_ar.R;
+import com.google.android.gms.vision.barcode.Barcode;
+
+import java.util.List;
+
+import info.androidhive.barcode.BarcodeReader;
+
+/**
+ * Created by thong.le on 10/18/2017.
+ */
+
+public class BarcodeActivity extends AppCompatActivity implements BarcodeReader.BarcodeReaderListener {
+
+    public static final String EXTRA_FOLDER = "extra_folder";
+
+    private BarcodeReader barcodeReader;
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        barcodeReader = (BarcodeReader) getSupportFragmentManager().findFragmentById(R.id.barcode_fragment);
+        barcodeReader.setBeepSoundFile("shutter.mp3");
+        barcodeReader.setListener(this);
+
+    }
+
+    @Override
+    public void onScanned(Barcode barcode) {
+        barcodeReader.playBeep();
+        Intent intent = new Intent();
+        intent.putExtra(EXTRA_FOLDER, barcode.displayValue);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+
+    @Override
+    public void onScannedMultiple(List<Barcode> barcodes) {
+
+    }
+
+    @Override
+    public void onBitmapScanned(SparseArray<Barcode> sparseArray) {
+
+    }
+
+    @Override
+    public void onScanError(String errorMessage) {
+
+    }
+}
--- app/src/main/java/com/company/sts_ar/view/menu/MenuActivity.java
+++ app/src/main/java/com/company/sts_ar/view/menu/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.company.sts_ar.view.menu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -25,9 +27,11 @@ import io.reactivex.schedulers.Schedulers;
 
 public class MenuActivity extends AppCompatActivity {
 
+    public static final int ACTION_SCAN = 121;
+
     private RecyclerView mRecyclerView;
     private MenuAdapter mAdapter;
-    private List<Project> mProjects =new ArrayList<>();
+    private List<Project> mProjects = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,11 +42,31 @@ public class MenuActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(new GridLayoutManager(this, 3));
         mRecyclerView.addItemDecoration(new GridDividerDecoration(getResources(), 4, true));
 
-        mAdapter = new MenuAdapter(this,mProjects);
+        mAdapter = new MenuAdapter(this, mProjects);
         mRecyclerView.setAdapter(mAdapter);
 
         Observable.fromCallable(() -> new GsonBuilder().create().fromJson(AppUtils.loadJSONFromAsset(this), Data.class).projects).subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(projects -> mAdapter.addItems(projects));
+
+        findViewById(R.id.fab)
+                .setOnClickListener(view -> startActivityForResult(new Intent(MenuActivity.this, BarcodeActivity.class), ACTION_SCAN));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == ACTION_SCAN) {
+            String folderName = data.getStringExtra(BarcodeActivity.EXTRA_FOLDER);
+            for (int i = 0; i < mProjects.size(); i++) {
+                Project project = mProjects.get(i);
+                if (project.folder.trim().equalsIgnoreCase(folderName.trim())) {
+                    Intent intent = new Intent(this, DetailActivity.class);
+                    intent.putExtra(DetailActivity.EXTRA_PROJECT, project);
+                    startActivity(intent);
+                    break;
+                }
+            }
+        }
     }
 }
